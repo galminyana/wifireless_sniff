858e6f38775a90c81ab63c4847f335a658c54aff
Guillem <46190684+galminyana@users.noreply.github.com>
2023-11-06 18:47:56 +0100

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,10 @@
+
+from scapy.all import *
+
+def PacketHandler (pkt) :
+	if pkt.haslayer(Dot11):
+		if pkt.type == 0 and pkt.subtype == 8:
+			print("AP MAC: %s with SSID: %s" %(pkt.addr2, pkt.info))
+
+
+sniff(iface="eth0", prn = PacketHandler)
