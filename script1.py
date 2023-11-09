6a20f386d3e753c73479dcbecef4fb1e0ba8b70d
Guillem <46190684+galminyana@users.noreply.github.com>
2023-11-09 15:43:12 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -2,9 +2,23 @@
 from scapy.all import *
 
 def PacketHandler (pkt) :
-	if pkt.haslayer(Dot11):
-		if pkt.type == 0 and pkt.subtype == 8:
-			print("AP MAC: %s with SSID: %s" %(pkt.addr2, pkt.info))
 
+        if pkt.haslayer(Dot11):
 
-sniff(iface="eth0", prn = PacketHandler)
+                if pkt.type == 0:
+
+                        if pkt.subtype == 4:
+                                print("MGMT-Probe Request: Device MAC %s asking for SSID: %s" %(pkt.addr2, pkt.info))
+
+                        elif pkt.subtype == 0:
+                                print("MGMT-Association Request: Device MAC %s trying to SSID %s" %(pkt.addr2, pkt.info))
+
+                        elif pkt.subtype == 2:
+                                printf("MGMT-Reassociation Request: Device MAC %s trying to SSID %s" %(pkt.addr2, pkt.info))
+
+                elif pkt.type == 1:
+
+                        if pkt.subtype == 11:
+                                print("CTRL-RTS: Device MAC %s is doing RTS" %(pkt.addr2))
+
+sniff(iface="wlan0mon", prn = PacketHandler)
